b54307221d955a3ab759d5c9181d5587c2fdf31d
Parfunc Account <parfunc@googlegroups.com>
2016-11-25 22:31:22 -0500

    code to run benchmarks on parallel code

--- command_line_runner.rkt
+++ command_line_runner.rkt
@@ -5,7 +5,9 @@
 (require racket/path racket/cmdline racket/file racket/system)
 (require/typed racket/os [gethostname (-> String)])
 
-(provide launch-benchmarks)
+(provide launch-benchmarks
+         launch-parallel-benchmarks
+         launch-ghc-parallel-benchmarks)
 
 (define host (list->string (for/list ([c (gethostname)] #:unless (char-numeric? c)) c)))
 
@@ -24,6 +26,26 @@
   (define csv (open-output-file outfile #:exists 'replace))
 
   (driver csv exec pass-name variant)
+  (system (format "cp ~a ~a" outfile (string-append destdir outfile)))
+  (printf "Output copied to ~a\n" destdir)
+  (close-output-port csv)) 
+
+(define (launch-parallel-benchmarks [exec : String] [pass-name : String] [variant : String] 
+                                    [threads : Integer])  
+  (define outfile (format "results_~a_~a.csv" variant (current-seconds)))
+  (define csv (open-output-file outfile #:exists 'replace))
+
+  (parallel-driver csv exec pass-name variant threads)
+  (system (format "cp ~a ~a" outfile (string-append destdir outfile)))
+  (printf "Output copied to ~a\n" destdir)
+  (close-output-port csv)) 
+
+(define (launch-ghc-parallel-benchmarks [exec : String] [pass-name : String] [variant : String] 
+                                        [threads : Integer])  
+  (define outfile (format "results_~a_~a.csv" variant (current-seconds)))
+  (define csv (open-output-file outfile #:exists 'replace))
+
+  (ghc-parallel-driver csv exec pass-name variant threads)
   (system (format "cp ~a ~a" outfile (string-append destdir outfile)))
   (printf "Output copied to ~a\n" destdir)
   (close-output-port csv)) 
