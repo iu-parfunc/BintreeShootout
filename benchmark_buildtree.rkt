744ad42c6dea6a1c3f66baa8922833aa33a09bd4
Ryan Newton <rrnewton@gmail.com>
2017-01-24 11:00:12 -0500

    minor tweaks for benchmarking

--- benchmark_buildtree.rkt
+++ benchmark_buildtree.rkt
@@ -14,20 +14,27 @@
 
 ;; NEW
 
-(launch-benchmarks "./treebench_mlton.exe build " PASSNAME "handwritten-mlton")
+;; (launch-benchmarks "./treebench_mlton.exe build " PASSNAME "handwritten-mlton")
 
-(launch-benchmarks "racket treebench.rkt build " PASSNAME "handwritten-racket")
+;; (launch-benchmarks "racket treebench.rkt build " PASSNAME "handwritten-racket")
 
-(launch-benchmarks "java treebench build " PASSNAME "handwritten-java")
+;; (launch-benchmarks "java treebench build " PASSNAME "handwritten-java")
 
-(launch-benchmarks "scheme --script treebench.ss build " PASSNAME "handwritten-chez")
+;; (launch-benchmarks "scheme --script treebench.ss build " PASSNAME "handwritten-chez")
 
-(launch-benchmarks "./treebench_rust.exe build " PASSNAME "handwritten-rust")
+;; (launch-benchmarks "./treebench_rust.exe build " PASSNAME "handwritten-rust")
 
-(launch-benchmarks "./treebench_ocaml.exe build " PASSNAME "handwritten-ocaml")
+;; (launch-benchmarks "./treebench_ocaml.exe build " PASSNAME "handwritten-ocaml")
 
-(launch-benchmarks "./treebench_c_packed.exe build " PASSNAME "handwritten-c-packed")
+;; (launch-benchmarks "./treebench_c_packed.exe build " PASSNAME "handwritten-c-packed")
 
-(launch-benchmarks "./treebench_c.exe build " PASSNAME "handwritten-c")
+;; (launch-benchmarks "./treebench_c.exe build " PASSNAME "handwritten-c")
 
 ;;(launch-benchmarks "./treebench_ghc_lazy.exe build " PASSNAME "handwritten-ghc")
+
+
+(launch-benchmarks "./buildtree_packed.exe " PASSNAME "treelang-c-packed")
+
+(launch-benchmarks "./buildtree_pointer.exe " PASSNAME "treelang-c-pointer")
+
+(launch-benchmarks "./buildtree_bumpalloc.exe " PASSNAME "treelang-c-bumpalloc")
--- benchmark_sumtree.rkt
+++ benchmark_sumtree.rkt
@@ -12,16 +12,23 @@
 
 ;;(launch-benchmarks "racket sumtree_gibbon.sexp " PASSNAME "treelang-racket")
 
-(launch-benchmarks "racket treebench.rkt sum " PASSNAME "handwritten-racket")
+;; (launch-benchmarks "racket treebench.rkt sum " PASSNAME "handwritten-racket")
 
-(launch-benchmarks "java treebench sum " PASSNAME "handwritten-java")
+;; (launch-benchmarks "java treebench sum " PASSNAME "handwritten-java")
 
-(launch-benchmarks "scheme --script treebench.ss sum " PASSNAME "handwritten-chez")
+;; (launch-benchmarks "scheme --script treebench.ss sum " PASSNAME "handwritten-chez")
 
-(launch-benchmarks "./treebench_rust.exe sum " PASSNAME "handwritten-rust")
+;; (launch-benchmarks "./treebench_rust.exe sum " PASSNAME "handwritten-rust")
 
-(launch-benchmarks "./treebench_ocaml.exe sum " PASSNAME "handwritten-ocaml")
+;; (launch-benchmarks "./treebench_ocaml.exe sum " PASSNAME "handwritten-ocaml")
 
-(launch-benchmarks "./treebench_ghc_lazy.exe sum " PASSNAME "handwritten-ghc")
+;; (launch-benchmarks "./treebench_ghc_lazy.exe sum " PASSNAME "handwritten-ghc")
 
-(launch-benchmarks "./treebench_mlton.exe sum " PASSNAME "handwritten-mlton")
+;; (launch-benchmarks "./treebench_mlton.exe sum " PASSNAME "handwritten-mlton")
+
+
+; (launch-benchmarks "./sumtree_packed.exe " PASSNAME "treelang-c-packed")
+
+(launch-benchmarks "./sumtree_pointer.exe " PASSNAME "treelang-c-pointer")
+
+;  (launch-benchmarks "./sumtree_bumpalloc.exe " PASSNAME "treelang-c-bumpalloc")
